1a5161a032ae302dd7f27f675511c4140400175c
Rohan Survase <rohansurvase10@gmail.com>
2023-10-07 23:34:40 +0530

    Done with Movie Controller

--- src/main/java/com/example/movies/Movie.java
+++ src/main/java/com/example/movies/Movie.java
@@ -11,31 +11,34 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
+@Document(collection = "movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collation = "movies")
 public class Movie {
 
-    @Id
-   private ObjectId _id;
-
+   @Id
+   private ObjectId id;
    private String imdbId;
-
    private String title;
-
-   private  String releaseDate;
-
+   private String releaseDate;
    private String trailerLink;
-
    private String poster;
-
-   private List<String> geners;
-
    private List<String> backdrops;
-
+   private List<String> genres;
    @DocumentReference
    private List<Review> reviews;
+
+//   public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres) {
+//      this.imdbId = imdbId;
+//      this.title = title;
+//      this.releaseDate = releaseDate;
+//      this.trailerLink = trailerLink;
+//      this.poster = poster;
+//      this.backdrops = backdrops;
+//      this.genres = genres;
+//   }
+
 
 
 }
--- src/main/java/com/example/movies/MovieController.java
+++ src/main/java/com/example/movies/MovieController.java
@@ -1,4 +1,32 @@
 package com.example.movies;
 
+
+import org.apache.coyote.Response;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/movie")
 public class MovieController {
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<Movie>>  singleMovie(@PathVariable String id){
+        return new ResponseEntity<Optional<Movie>>(service.singleMovieWithID(id),HttpStatus.OK);
+    }
 }
